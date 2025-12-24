5361685b7597e98c1285442485eb19a53b4a4a9e
himanshu2285 <himanshu2k05m@gmail.com>
2025-12-24 23:55:02 +0530

    Created a flow in LangFlow and Uses API for local access on vs code

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import os
+# from headers import headers
+import uuid
+
+url = "http://localhost:7860/api/v1/run/375a7d91-b3ba-482e-8d5a-43914c33facb"  # The complete API endpoint URL for this flow
+
+# Request payload configuration
+payload = {
+    "output_type": "chat",
+    "input_type": "chat",
+    "input_value": "Give me a 5 day workout plan for building muscle. My body weight is 60kg"
+}
+payload["session_id"] = str(uuid.uuid4())
+
+
+
+try:
+    # Send API request
+    # response = requests.request("POST", url, json=payload, headers=headers)
+    response = requests.request("POST", url, json=payload)
+    response.raise_for_status()  # Raise exception for bad status codes
+
+    # Print response
+    print(response.text)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error making API request: {e}")
+except ValueError as e:
+    print(f"Error parsing response: {e}")
